403fb1d9149d2eb498b2223efacfd87caaffef8e
Frank <francis.kipchumba@moringaschool.com>
2024-01-26 05:00:38 +0300

    Style the product page
     - style and make Product page responsive
     - style and add responsiveness to productd description and product detail page

--- src/components/products/ProductDescription.jsx
+++ src/components/products/ProductDescription.jsx
@@ -16,30 +16,16 @@ const ProductDescription = () => {
   const relatedProducts = products.filter((p) => p.id === product.id);
 
   return (
-    <div className="bg-gray-100 mt-16">
-      <div className="flex-grow">
-        <div className="flex flex-col sm:flex-row ml-80 mr-96">
-          <div className="sm:w-1/3 p-4 ml-4 sm:ml-0">
-            <div className="mb-10 bg-gray-200 p-6 flex justify-center">
-              <Link to="/contact">
-                <button className="bg-blue-800 text-white text-2xl px-14 py-7 hover:bg-blue-700">
-                  Get a Free Quote
-                </button>
-              </Link>
-            </div>
-            <div className="bg-gray-200 px-16 py-24">
-              <h2 className="text-4xl font-bold mb-4">Contact Us</h2>
-              <p className="text-black text-xl">Office 308, Lunga Lunga</p>
-              <p className="text-black text-xl">Square</p>
-              <p className="text-xl mb-4">Industrial Area</p>
-              <p className="text-xl">+254 750 520 179</p>
-              <p className="text-xl mb-4">+254 717 520 179</p>
-              <p className="text-xl">info@hachfeld.co.ke</p>
-              <p className="text-xl">sales@hachfeld.co.ke</p>
-              <p className="text-xl">www.hachfeld.co.ke</p>
-            </div>
+    <div className="bg-gray-100 mt-16 container mx-auto px-4">
+      <div className="flex flex-col md:flex-row items-start justify-between">
+        <div className="p-4 md:w-1/3">
+          <div className="mb-10 bg-gray-200 p-6 flex justify-center">
+            <Link to="/contact"><button className="bg-blue-800 text-white text-2xl px-8 py-4 hover:bg-blue-700 rounded-full">
+              Get a Free Quote
+            </button></Link>
           </div>
-          <div className="sm:w-2/3 p-4">
+        </div>
+        <div className="p-4 md:w-2/3">
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 mt-4">
               <div className="bg-white p-6">
                 <img src={product.imageUrl} alt={product.name} className="w-full object-cover mb-4 h-96" />
@@ -49,9 +35,13 @@ const ProductDescription = () => {
                 <p className="text-2xl mt-4">{product.description}</p>
               </div>
             </div>
+            
           </div>
         </div>
-        <div className="mt-8">
+        <div className='flex flex-col md:flex-row items-end justify-between'>
+
+        
+        <div className="p-4">
           <h2 className="text-4xl font-semibold text-left mb-4 ml-96">Description</h2>
           <div className="text-xl mt-8">
             <p className="text-2xl text-left ml-96">
@@ -65,7 +55,7 @@ const ProductDescription = () => {
         </div>
         <div className="mt-8">
           <h2 className="text-3xl font-semibold text-left mb-4 ml-96">Related Products</h2>
-          <div className="grid grid-cols-4 gap-2">
+          <div className="grid gap-2">
             {relatedProducts.map((relatedProduct) => (
               <div key={relatedProduct.id} className="bg-white p-4 w-96 ml-96">
                 <img src={relatedProduct.imageUrl} alt={relatedProduct.name} className="w-full object-cover mb-2 h-96" />
@@ -73,7 +63,7 @@ const ProductDescription = () => {
               </div>
             ))}
           </div>
-        </div>
+      </div>
       </div>
       <Footer />
     </div>
--- src/components/products/ProductDetailPage.jsx
+++ src/components/products/ProductDetailPage.jsx
@@ -15,30 +15,17 @@ const ProductDetailPage = () => {
   }
 
   return (
-    <div className="bg-gray-100 mt-16">
-      <div className='flex-grow'>
-        <div className="flex flex-col sm:flex-row ml-80 mr-96">
-          <div className="sm:w-1/3 p-4 ml-4 sm:ml-0">
-            <div className="mb-10 bg-gray-200 p-6 flex justify-center">
-            <Link to="/contact"><button className="bg-blue-800 text-white text-2xl px-14 py-7 hover:bg-blue-700">
+    <div className="bg-gray-100 mt-16 container mx-auto px-4">
+      <div className="flex flex-col md:flex-row items-start justify-between">
+        <div className="p-4 md:w-1/3">
+          <div className="mb-10 bg-gray-200 p-6 flex justify-center">
+            <Link to="/contact"><button className="bg-blue-800 text-white text-2xl px-8 py-4 hover:bg-blue-700 rounded-full">
               Get a Free Quote
             </button></Link>
-            </div>
-            <div className="bg-gray-200 px-16 py-24">
-              <h2 className="text-4xl font-bold mb-4 ">Contact Us</h2>
-              <p className="text-black text-xl">Office 308, Lunga Lunga</p>
-              <p className="text-black text-xl">Square,</p>
-              <p className="text-xl mb-4">Industrial Area,</p>
-              <p className="text-xl">+254 750 520 179</p>
-              <p className="text-xl mb-4">+254 717 520 179</p>
-              <p className="text-xl">info@hachfeld.co.ke</p>
-              <p className="text-xl">sales@hachfeld.co.ke</p>
-              <p className="text-xl">www.hachfeld.co.ke</p>
-            </div>
           </div>
-          <div className="sm:w-2/3 p-4">
-         
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 mt-4">
+        </div>
+        <div className="p-4 md:w-2/3">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-4">
             {Products.map((product) => (
               <div key={product.id} className="bg-white p-6">
                <Link to={`/product/${product.id}/${product.name}`}> 
@@ -50,7 +37,6 @@ const ProductDetailPage = () => {
             ))}
             </div>
           </div>
-        </div>
       </div>
       <Footer />
     </div>
