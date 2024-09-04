6deb30a548ce8e24cf572a38b774798d799bebc5
francky <francky@localhost.localdomain>
2024-09-04 20:49:42 +0300

    update the landing page writeup

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -17,7 +17,7 @@ const HeroSection = () => {
       <div className="flex flex-col items-center justify-center h-full">
         <h1 className="font-bold mb-4 text-4xl md:text-6xl pt-20">Supporting your Analytical Needs.</h1>
         <p className="text-xl md:text-4xl max-w-md mx-auto mb-8 md:text-center lg:flex lg:items-start lg:justify-start lg:max-w-full lg:mx-0">
-          Analytical laboratory & process equipment, reagents, and consumables.
+          TO ENSURE QUALITY AND OPERATIONAL EXCELLENCE IN LABS.
         </p>
         <Link to="/products">
           <button className="bg-blue-800 text-white hover:bg-blue-700 font-bold py-2 px-4 rounded-full text-lg md:text-xl">
